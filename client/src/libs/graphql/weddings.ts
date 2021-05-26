dde3979abd3cb6d2d50855a34c8286462df1f202
kokily <kokily@naver.com>
2021-05-26 16:02:15 +0900

    Client wedding gql Read wedding detail ing...

--- client/src/libs/graphql/weddings.ts
+++ client/src/libs/graphql/weddings.ts
@@ -3,140 +3,128 @@ import { gql } from '@apollo/client';
 // Add Wedding API
 export const ADD_WEDDING = gql`
   mutation AddWedding(
-    $husband: String!
-    $bride: String!
-    $reserve_pay: Int!
-    $husband_rental: Int!
-    $bride_rental: Int!
-    $sum_rental: Int!
-    $husband_company: Int!
-    $bride_company: Int!
-    $sum_company: Int!
-    $husband_add: Int!
-    $bride_add: Int!
-    $sum_add: Int!
-    $husband_today: Int!
-    $bride_today: Int!
-    $sum_today: Int!
-    $husband_bouquet: Int!
-    $bride_bouquet: Int!
-    $sum_bouquet: Int!
-    $husband_ceremony: Int!
-    $bride_ceremony: Int!
-    $sum_ceremony: Int!
-    $husband_hanbok: Int!
-    $bride_hanbok: Int!
-    $sum_hanbok: Int!
-    $husband_play: Int!
-    $bride_play: Int!
-    $sum_play: Int!
-    $husband_anthem: Int!
-    $bride_anthem: Int!
-    $sum_anthem: Int!
-    $husband_moderator: Int!
-    $bride_moderator: Int!
-    $sum_moderator: Int!
-    $husband_officiate: Int!
-    $bride_officiate: Int!
-    $sum_officiate: Int!
-    $husband_etc: Int!
-    $bride_etc: Int!
-    $sum_etc: Int!
-    $husband_conv: Int!
-    $bride_conv: Int!
-    $sum_conv: Int!
-    $husband_wedding: Int!
-    $bride_wedding: Int!
-    $sum_wedding: Int!
-    $meals_price: Int!
-    $husband_num: Int!
-    $bride_num: Int!
-    $sum_num: Int!
-    $husband_meal: Int!
-    $bride_meal: Int!
-    $sum_meal: Int!
-    $present_price: Int!
-    $husband_present_num: Int!
-    $bride_present_num: Int!
-    $sum_present_num: Int!
-    $husband_present: Int!
-    $bride_present: Int!
-    $sum_present: Int!
-    $meal: String!
-    $reserve: String!
-    $present: String!
-    $husband_reserve: Int!
-    $bride_reserve: Int!
+    $husband_name: String!
+    $bride_name: String!
     $wedding_at: String!
     $event_at: String!
+    $company_husband: Int!
+    $company_bride: Int!
+    $rooftop_husband: Int!
+    $rooftop_bride: Int!
+    $owner_woman_husband: Int!
+    $owner_woman_bride: Int!
+    $owner_man_husband: Int!
+    $owner_man_bride: Int!
+    $select_husband: Int!
+    $select_bride: Int!
+    $frame_husband: Int!
+    $frame_bride: Int!
+    $dress_husband: Int!
+    $dress_bride: Int!
+    $hairpin_husband: Int!
+    $hairpin_bride: Int!
+    $wig_husband: Int!
+    $wig_bride: Int!
+    $video_husband: Int!
+    $video_bride: Int!
+    $etc_husband: Int!
+    $etc_bride: Int!
+    $rental_husband: Int!
+    $rental_bride: Int!
+    $sword_husband: Int!
+    $sword_bride: Int!
+    $glove_husband: Int!
+    $glove_bride: Int!
+    $bouquet_husband: Int!
+    $bouquet_bride: Int!
+    $ceremony_husband: Int!
+    $ceremony_bride: Int!
+    $play_husband: Int!
+    $play_bride: Int!
+    $anthem_husband: Int!
+    $anthem_bride: Int!
+    $moderator_husband: Int!
+    $moderator_bride: Int!
+    $officiate_husband: Int!
+    $officiate_bride: Int!
+    $hanbok_pre_husband: Int!
+    $hanbok_pre_bride: Int!
+    $hanbok_post_husband: Int!
+    $hanbok_post_bride: Int!
+    $meals: String!
+    $meals_price: Int!
+    $meals_num_husband: Int!
+    $meals_num_bride: Int!
+    $present: String!
+    $present_price: Int!
+    $present_num_husband: Int!
+    $present_num_bride: Int!
+    $reserve: String!
+    $reserve_pay: Int!
+    $reserve_husband: Int!
+    $reserve_bride: Int!
   ) {
     AddWedding(
-      husband: $husband
-      bride: $bride
-      reserve_pay: $reserve_pay
-      husband_rental: $husband_rental
-      bride_rental: $bride_rental
-      sum_rental: $sum_rental
-      husband_company: $husband_company
-      bride_company: $bride_company
-      sum_company: $sum_company
-      husband_add: $husband_add
-      bride_add: $bride_add
-      sum_add: $sum_add
-      husband_today: $husband_today
-      bride_today: $bride_today
-      sum_today: $sum_today
-      husband_bouquet: $husband_bouquet
-      bride_bouquet: $bride_bouquet
-      sum_bouquet: $sum_bouquet
-      husband_ceremony: $husband_ceremony
-      bride_ceremony: $bride_ceremony
-      sum_ceremony: $sum_ceremony
-      husband_hanbok: $husband_hanbok
-      bride_hanbok: $bride_hanbok
-      sum_hanbok: $sum_hanbok
-      husband_play: $husband_play
-      bride_play: $bride_play
-      sum_play: $sum_play
-      husband_anthem: $husband_anthem
-      bride_anthem: $bride_anthem
-      sum_anthem: $sum_anthem
-      husband_moderator: $husband_moderator
-      bride_moderator: $bride_moderator
-      sum_moderator: $sum_moderator
-      husband_officiate: $husband_officiate
-      bride_officiate: $bride_officiate
-      sum_officiate: $sum_officiate
-      husband_etc: $husband_etc
-      bride_etc: $bride_etc
-      sum_etc: $sum_etc
-      husband_conv: $husband_conv
-      bride_conv: $bride_conv
-      sum_conv: $sum_conv
-      husband_wedding: $husband_wedding
-      bride_wedding: $bride_wedding
-      sum_wedding: $sum_wedding
-      meals_price: $meals_price
-      husband_num: $husband_num
-      bride_num: $bride_num
-      sum_num: $sum_num
-      husband_meal: $husband_meal
-      bride_meal: $bride_meal
-      sum_meal: $sum_meal
-      present_price: $present_price
-      husband_present_num: $husband_present_num
-      bride_present_num: $bride_present_num
-      sum_present_num: $sum_present_num
-      husband_present: $husband_present
-      bride_present: $bride_present
-      sum_present: $sum_present
-      meal: $meal
-      reserve: $reserve
-      present: $present
-      husband_reserve: $husband_reserve
-      bride_reserve: $bride_reserve
+      husband_name: $husband_name
+      bride_name: $bride_name
       wedding_at: $wedding_at
       event_at: $event_at
+      company_husband: $company_husband
+      company_bride: $company_bride
+      rooftop_husband: $rooftop_husband
+      rooftop_bride: $rooftop_bride
+      owner_woman_husband: $owner_woman_husband
+      owner_woman_bride: $owner_woman_bride
+      owner_man_husband: $owner_man_husband
+      owner_man_bride: $owner_man_bride
+      select_husband: $select_husband
+      select_bride: $select_bride
+      frame_husband: $frame_husband
+      frame_bride: $frame_bride
+      dress_husband: $dress_husband
+      dress_bride: $dress_bride
+      hairpin_husband: $hairpin_husband
+      hairpin_bride: $hairpin_bride
+      wig_husband: $wig_husband
+      wig_bride: $wig_bride
+      video_husband: $video_husband
+      video_bride: $video_bride
+      etc_husband: $etc_husband
+      etc_bride: $etc_bride
+      rental_husband: $rental_husband
+      rental_bride: $rental_bride
+      sword_husband: $sword_husband
+      sword_bride: $sword_bride
+      glove_husband: $glove_husband
+      glove_bride: $glove_bride
+      bouquet_husband: $bouquet_husband
+      bouquet_bride: $bouquet_bride
+      ceremony_husband: $ceremony_husband
+      ceremony_bride: $ceremony_bride
+      play_husband: $play_husband
+      play_bride: $play_bride
+      anthem_husband: $anthem_husband
+      anthem_bride: $anthem_bride
+      moderator_husband: $moderator_husband
+      moderator_bride: $moderator_bride
+      officiate_husband: $officiate_husband
+      officiate_bride: $officiate_bride
+      hanbok_pre_husband: $hanbok_pre_husband
+      hanbok_pre_bride: $hanbok_pre_bride
+      hanbok_post_husband: $hanbok_post_husband
+      hanbok_post_bride: $hanbok_post_bride
+      meals: $meals
+      meals_price: $meals_price
+      meals_num_husband: $meals_num_husband
+      meals_num_bride: $meals_num_bride
+      present: $present
+      present_price: $present_price
+      present_num_husband: $present_num_husband
+      present_num_bride: $present_num_bride
+      reserve: $reserve
+      reserve_pay: $reserve_pay
+      reserve_husband: $reserve_husband
+      reserve_bride: $reserve_bride
     ) {
       ok
       error
@@ -146,80 +134,18 @@ export const ADD_WEDDING = gql`
 
 // List Weddings API
 export const LIST_WEDDINGS = gql`
-  query ListWeddings($date: String, $cursor: ID) {
+  query ListWeddings($date: String!, $cursor: ID) {
     ListWeddings(date: $date, cursor: $cursor) {
       ok
       error
       weddings {
         id
-        husband
-        bride
-        reserve_pay
-        husband_rental
-        bride_rental
-        sum_rental
-        husband_company
-        bride_company
-        sum_company
-        husband_add
-        bride_add
-        sum_add
-        husband_today
-        bride_today
-        sum_today
-        husband_bouquet
-        bride_bouquet
-        sum_bouquet
-        husband_ceremony
-        bride_ceremony
-        sum_ceremony
-        husband_hanbok
-        bride_hanbok
-        sum_hanbok
-        husband_play
-        bride_play
-        sum_play
-        husband_anthem
-        bride_anthem
-        sum_anthem
-        husband_moderator
-        bride_moderator
-        sum_moderator
-        husband_officiate
-        bride_officiate
-        sum_officiate
-        husband_etc
-        bride_etc
-        sum_etc
-        husband_conv
-        bride_conv
-        sum_conv
-        husband_wedding
-        bride_wedding
-        sum_wedding
-        meals_price
-        husband_num
-        bride_num
-        sum_num
-        husband_meal
-        bride_meal
-        sum_meal
-        present_price
-        husband_present_num
-        bride_present_num
-        sum_present_num
-        husband_present
-        bride_present
-        sum_present
-        meal
-        reserve
-        present
-        husband_reserve
-        bride_reserve
-        created_at
-        updated_at
+        husband_name
+        bride_name
         wedding_at
         event_at
+        created_at
+        updated_at
       }
     }
   }
@@ -232,304 +158,35 @@ export const READ_WEDDING = gql`
       ok
       error
       wedding {
-        id
-        husband
-        bride
-        reserve_pay
-        husband_rental
-        bride_rental
-        sum_rental
-        husband_company
-        bride_company
-        sum_company
-        husband_add
-        bride_add
-        sum_add
-        husband_today
-        bride_today
-        sum_today
-        husband_bouquet
-        bride_bouquet
-        sum_bouquet
-        husband_ceremony
-        bride_ceremony
-        sum_ceremony
-        husband_hanbok
-        bride_hanbok
-        sum_hanbok
-        husband_play
-        bride_play
-        sum_play
-        husband_anthem
-        bride_anthem
-        sum_anthem
-        husband_moderator
-        bride_moderator
-        sum_moderator
-        husband_officiate
-        bride_officiate
-        sum_officiate
-        husband_etc
-        bride_etc
-        sum_etc
-        husband_conv
-        bride_conv
-        sum_conv
-        husband_wedding
-        bride_wedding
-        sum_wedding
-        meals_price
-        husband_num
-        bride_num
-        sum_num
-        husband_meal
-        bride_meal
-        sum_meal
-        present_price
-        husband_present_num
-        bride_present_num
-        sum_present_num
-        husband_present
-        bride_present
-        sum_present
-        meal
-        reserve
-        present
-        husband_reserve
-        bride_reserve
-        created_at
-        updated_at
-        wedding_at
-        event_at
+
+      }
+      company {
+
+      }
+      convention {
+
+      }
+      event {
+
+      }
+      hanbok {
+
+      }
+      meal {
+
+      }
+      present {
+
+      }
+      reserve {
+        
       }
     }
   }
 `;
 
 // Remove Wedding API
-export const REMOVE_WEDDING = gql`
-  mutation RemoveWedding($id: ID!) {
-    RemoveWedding(id: $id) {
-      ok
-      error
-    }
-  }
-`;
+export const REMOVE_WEDDING = gql``;
 
 // Update Wedding API
-export const UPDATE_WEDDING = gql`
-  mutation UpdateWedding(
-    $id: ID!
-    $husband: String!
-    $bride: String!
-    $reserve_pay: Int!
-    $husband_rental: Int!
-    $bride_rental: Int!
-    $sum_rental: Int!
-    $husband_company: Int!
-    $bride_company: Int!
-    $sum_company: Int!
-    $husband_add: Int!
-    $bride_add: Int!
-    $sum_add: Int!
-    $husband_today: Int!
-    $bride_today: Int!
-    $sum_today: Int!
-    $husband_bouquet: Int!
-    $bride_bouquet: Int!
-    $sum_bouquet: Int!
-    $husband_ceremony: Int!
-    $bride_ceremony: Int!
-    $sum_ceremony: Int!
-    $husband_hanbok: Int!
-    $bride_hanbok: Int!
-    $sum_hanbok: Int!
-    $husband_play: Int!
-    $bride_play: Int!
-    $sum_play: Int!
-    $husband_anthem: Int!
-    $bride_anthem: Int!
-    $sum_anthem: Int!
-    $husband_moderator: Int!
-    $bride_moderator: Int!
-    $sum_moderator: Int!
-    $husband_officiate: Int!
-    $bride_officiate: Int!
-    $sum_officiate: Int!
-    $husband_etc: Int!
-    $bride_etc: Int!
-    $sum_etc: Int!
-    $husband_conv: Int!
-    $bride_conv: Int!
-    $sum_conv: Int!
-    $husband_wedding: Int!
-    $bride_wedding: Int!
-    $sum_wedding: Int!
-    $meals_price: Int!
-    $husband_num: Int!
-    $bride_num: Int!
-    $sum_num: Int!
-    $husband_meal: Int!
-    $bride_meal: Int!
-    $sum_meal: Int!
-    $present_price: Int!
-    $husband_present_num: Int!
-    $bride_present_num: Int!
-    $sum_present_num: Int!
-    $husband_present: Int!
-    $bride_present: Int!
-    $sum_present: Int!
-    $meal: String!
-    $reserve: String!
-    $present: String!
-    $husband_reserve: Int!
-    $bride_reserve: Int!
-    $wedding_at: String!
-    $event_at: String!
-  ) {
-    UpdateWedding(
-      id: $id
-      husband: $husband
-      bride: $bride
-      reserve_pay: $reserve_pay
-      husband_rental: $husband_rental
-      bride_rental: $bride_rental
-      sum_rental: $sum_rental
-      husband_company: $husband_company
-      bride_company: $bride_company
-      sum_company: $sum_company
-      husband_add: $husband_add
-      bride_add: $bride_add
-      sum_add: $sum_add
-      husband_today: $husband_today
-      bride_today: $bride_today
-      sum_today: $sum_today
-      husband_bouquet: $husband_bouquet
-      bride_bouquet: $bride_bouquet
-      sum_bouquet: $sum_bouquet
-      husband_ceremony: $husband_ceremony
-      bride_ceremony: $bride_ceremony
-      sum_ceremony: $sum_ceremony
-      husband_hanbok: $husband_hanbok
-      bride_hanbok: $bride_hanbok
-      sum_hanbok: $sum_hanbok
-      husband_play: $husband_play
-      bride_play: $bride_play
-      sum_play: $sum_play
-      husband_anthem: $husband_anthem
-      bride_anthem: $bride_anthem
-      sum_anthem: $sum_anthem
-      husband_moderator: $husband_moderator
-      bride_moderator: $bride_moderator
-      sum_moderator: $sum_moderator
-      husband_officiate: $husband_officiate
-      bride_officiate: $bride_officiate
-      sum_officiate: $sum_officiate
-      husband_etc: $husband_etc
-      bride_etc: $bride_etc
-      sum_etc: $sum_etc
-      husband_conv: $husband_conv
-      bride_conv: $bride_conv
-      sum_conv: $sum_conv
-      husband_wedding: $husband_wedding
-      bride_wedding: $bride_wedding
-      sum_wedding: $sum_wedding
-      meals_price: $meals_price
-      husband_num: $husband_num
-      bride_num: $bride_num
-      sum_num: $sum_num
-      husband_meal: $husband_meal
-      bride_meal: $bride_meal
-      sum_meal: $sum_meal
-      present_price: $present_price
-      husband_present_num: $husband_present_num
-      bride_present_num: $bride_present_num
-      sum_present_num: $sum_present_num
-      husband_present: $husband_present
-      bride_present: $bride_present
-      sum_present: $sum_present
-      meal: $meal
-      reserve: $reserve
-      present: $present
-      husband_reserve: $husband_reserve
-      bride_reserve: $bride_reserve
-      wedding_at: $wedding_at
-      event_at: $event_at
-    ) {
-      ok
-      error
-      wedding {
-        id
-        husband
-        bride
-        reserve_pay
-        husband_rental
-        bride_rental
-        sum_rental
-        husband_company
-        bride_company
-        sum_company
-        husband_add
-        bride_add
-        sum_add
-        husband_today
-        bride_today
-        sum_today
-        husband_bouquet
-        bride_bouquet
-        sum_bouquet
-        husband_ceremony
-        bride_ceremony
-        sum_ceremony
-        husband_hanbok
-        bride_hanbok
-        sum_hanbok
-        husband_play
-        bride_play
-        sum_play
-        husband_anthem
-        bride_anthem
-        sum_anthem
-        husband_moderator
-        bride_moderator
-        sum_moderator
-        husband_officiate
-        bride_officiate
-        sum_officiate
-        husband_etc
-        bride_etc
-        sum_etc
-        husband_conv
-        bride_conv
-        sum_conv
-        husband_wedding
-        bride_wedding
-        sum_wedding
-        meals_price
-        husband_num
-        bride_num
-        sum_num
-        husband_meal
-        bride_meal
-        sum_meal
-        present_price
-        husband_present_num
-        bride_present_num
-        sum_present_num
-        husband_present
-        bride_present
-        sum_present
-        meal
-        reserve
-        present
-        husband_reserve
-        bride_reserve
-        created_at
-        updated_at
-        wedding_at
-        event_at
-        user_id
-      }
-    }
-  }
-`;
+export const UPDATE_WEDDING = gql``;
